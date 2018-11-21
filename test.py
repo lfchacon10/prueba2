512103b3f4c6d2fd2a1cb2434b4a54609397dfff
lfchacon10 <lf.chacon10@uniandes.edu.co>
2018-11-21 18:20:17 +0000

    Prueba4

--- test.py
+++ test.py
@@ -5,3 +5,5 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 print("Hola mundo")
+#CAMBIO ALEATORIO
+print ("Este es un numero superhipermegarandom: " ,np.random.random())
